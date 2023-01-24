852ae74c190f321d7ed8642814ccec9694e90a95
TG <trygod.nwakwasi@decagon.dev>
2023-01-24 22:12:59 +0100

    created the DealRepository interface

---
+++ src/main/java/com/progresssoft/clustereddatawarehouse/repository/DealRepository.java
@@ -0,0 +1,8 @@
+package com.progresssoft.clustereddatawarehouse.repository;
+
+import com.progresssoft.clustereddatawarehouse.entity.Deal;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DealRepository extends JpaRepository<Deal, Long> {
+
+}
