796484169ba2564c30b251f8008c36b6307cd2d4
TG <trygod.nwakwasi@decagon.dev>
2023-01-25 20:02:51 +0100

    added the @Repository annotation

--- src/main/java/com/progresssoft/clustereddatawarehouse/repository/DealRepository.java
+++ src/main/java/com/progresssoft/clustereddatawarehouse/repository/DealRepository.java
@@ -2,7 +2,9 @@ package com.progresssoft.clustereddatawarehouse.repository;
 
 import com.progresssoft.clustereddatawarehouse.entity.Deal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DealRepository extends JpaRepository<Deal, Long> {
 
 }
