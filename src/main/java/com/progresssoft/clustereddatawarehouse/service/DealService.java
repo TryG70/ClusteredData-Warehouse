6b8912497db8595ea0419f91e5b58f46e0f4d1a3
TG <trygod.nwakwasi@decagon.dev>
2023-01-25 19:51:54 +0100

    created the DealService interface

---
+++ src/main/java/com/progresssoft/clustereddatawarehouse/service/DealService.java
@@ -0,0 +1,8 @@
+package com.progresssoft.clustereddatawarehouse.service;
+
+import com.progresssoft.clustereddatawarehouse.dto.DealDto;
+
+public interface DealService {
+
+    DealDto saveFXDeal(DealDto dealDTO);
+}
