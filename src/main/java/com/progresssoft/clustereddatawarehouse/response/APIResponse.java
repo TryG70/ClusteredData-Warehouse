3a7120c9b1ccdade22b6d1fe84aec8b52d634d7e
TG <trygod.nwakwasi@decagon.dev>
2023-01-25 20:01:54 +0100

    created the APIResponse class

---
+++ src/main/java/com/progresssoft/clustereddatawarehouse/response/APIResponse.java
@@ -0,0 +1,17 @@
+package com.progresssoft.clustereddatawarehouse.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Builder
+@Data
+public class APIResponse<T> {
+
+    private String message;
+    private LocalDateTime time;
+    private T dto;
+}
