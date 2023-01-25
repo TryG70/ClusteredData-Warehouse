bb020f374533438ce5c4461cfea68ddc86cd7739
TG <trygod.nwakwasi@decagon.dev>
2023-01-25 21:25:28 +0100

    added an sql method

--- src/main/java/com/progresssoft/clustereddatawarehouse/repository/DealRepository.java
+++ src/main/java/com/progresssoft/clustereddatawarehouse/repository/DealRepository.java
@@ -4,7 +4,10 @@ import com.progresssoft.clustereddatawarehouse.entity.Deal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface DealRepository extends JpaRepository<Deal, Long> {
 
+    Optional<Deal> findByDealUniqueId(String dealUniqueId);
 }
