811f4b2d8266b21e1d34e47128b0a09d15c98b8e
TG <trygod.nwakwasi@decagon.dev>
2023-01-27 00:47:12 +0100

    added the SameISOCodeException method

--- src/main/java/com/progresssoft/clustereddatawarehouse/controller/ExceptionController.java
+++ src/main/java/com/progresssoft/clustereddatawarehouse/controller/ExceptionController.java
@@ -1,13 +1,15 @@
 package com.progresssoft.clustereddatawarehouse.controller;
 
 import com.progresssoft.clustereddatawarehouse.exception.DealAlreadyExistsException;
-import com.progresssoft.clustereddatawarehouse.exception.ExceptionResponse;
+import com.progresssoft.clustereddatawarehouse.response.ExceptionResponse;
+import com.progresssoft.clustereddatawarehouse.exception.SameISOCodeException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.FORBIDDEN;
 
 @ControllerAdvice
@@ -18,5 +20,12 @@ public class ExceptionController extends ResponseEntityExceptionHandler {
 
         ExceptionResponse exceptionResponse = new ExceptionResponse(exception.getMessage(), FORBIDDEN);
         return new ResponseEntity<>(exceptionResponse,FORBIDDEN);
+    }
+
+    @ExceptionHandler(SameISOCodeException.class)
+    public ResponseEntity<Object> sameISOCodeException(SameISOCodeException exception) {
+
+        ExceptionResponse exceptionResponse = new ExceptionResponse(exception.getMessage(), BAD_REQUEST);
+        return new ResponseEntity<>(exceptionResponse,BAD_REQUEST);
     }
 }
