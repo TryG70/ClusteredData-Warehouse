b229aa4fb2deff0810c442adfeddf9ae8ca739f0
TG <trygod.nwakwasi@decagon.dev>
2023-01-29 23:10:52 +0100

    wrote service implementation tests for getDealByDealUniqueId and retrieveFxDeal

--- src/test/java/com/progresssoft/clustereddatawarehouse/serviceImpl/DealServiceImplTest.java
+++ src/test/java/com/progresssoft/clustereddatawarehouse/serviceImpl/DealServiceImplTest.java
@@ -3,8 +3,10 @@ package com.progresssoft.clustereddatawarehouse.serviceImpl;
 import com.progresssoft.clustereddatawarehouse.dto.DealDto;
 import com.progresssoft.clustereddatawarehouse.entity.Deal;
 import com.progresssoft.clustereddatawarehouse.exception.DealAlreadyExistsException;
+import com.progresssoft.clustereddatawarehouse.exception.FxDealNotFoundException;
 import com.progresssoft.clustereddatawarehouse.exception.SameISOCodeException;
 import com.progresssoft.clustereddatawarehouse.repository.DealRepository;
+import com.progresssoft.clustereddatawarehouse.response.APIResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -38,14 +40,13 @@ class DealServiceImplTest {
 
     @Test
     void failed_testSaveFXDeal_dealUniqueId_alreadyExists() {
-        // Setup
+
         final DealDto dealDTO = new DealDto("TFGHU789YH", "EUR", "USD",
                 new BigDecimal("50.00"));
 
         final Deal savedDeal = new Deal(1L, "TFGHU789YH", Currency.getInstance("EUR"), Currency.getInstance("USD"), time, new BigDecimal("50.00"));
 
         when(mockDealRepository.findByDealUniqueId("TFGHU789YH")).thenReturn(Optional.of(savedDeal));
-
 
         assertThrows(DealAlreadyExistsException.class, () -> dealServiceImplUnderTest.saveFXDeal(dealDTO));
         verify(mockDealRepository, times(1)).findByDealUniqueId(dealDTO.getDealUniqueId());
@@ -55,24 +56,21 @@ class DealServiceImplTest {
 
     @Test
     void failed_testSaveFXDeal_sameISOCodeException() {
-        // Setup
+
         final DealDto dealDTO = new DealDto("TFGHU789YH", "EUR", "EUR",
                 new BigDecimal("50.00"));
 
         assertThrows(SameISOCodeException.class, () -> dealServiceImplUnderTest.saveFXDeal(dealDTO));
-
-
     }
 
     @Test
     void succesful_testSaveFXDeal() {
-        // Setup
+
         final DealDto dealDTO = new DealDto("TFGHU789YH", "EUR", "USD",
                 new BigDecimal("50.00"));
 
         when(mockDealRepository.findByDealUniqueId("TFGHU789YH")).thenReturn(Optional.empty());
 
-        // Configure DealRepository.save(...).
         final Deal deal = Deal.builder()
                 .dealUniqueId("TFGHU789YH")
                 .fromCurrencyISOCode(Currency.getInstance("EUR"))
@@ -83,17 +81,68 @@ class DealServiceImplTest {
 
         when(mockDealRepository.save(deal)).thenReturn(savedDeal);
 
-        // Run the test
         var actual = dealServiceImplUnderTest.saveFXDeal(dealDTO);
 
-
-
-        // Verify the results
         assertThat(actual.getDto().getDealUniqueId()).isEqualTo(dealDTO.getDealUniqueId());
         assertThat(actual.getDto().getFromCurrencyISOCode()).isEqualTo(dealDTO.getFromCurrencyISOCode());
         assertThat(actual.getDto().getToCurrencyISOCode()).isEqualTo(dealDTO.getToCurrencyISOCode());
         assertThat(actual.getDto().getDealAmount()).isEqualTo(dealDTO.getDealAmount());
         verify(mockDealRepository, times(1)).findByDealUniqueId(dealDTO.getDealUniqueId());
         verify(mockDealRepository, times(1)).save(deal);
+    }
+
+    @Test
+    void testGetDealByDealUniqueId() {
+
+        final Deal expectedResult = new Deal(1L, "TFGHU789YH", Currency.getInstance("EUR"),
+                Currency.getInstance("USD"), time, new BigDecimal("50.00"));
+
+        final Optional<Deal> optionalDeal = Optional.of(
+                new Deal(1L, "TFGHU789YH", Currency.getInstance("EUR"), Currency.getInstance("USD"),
+                        time, new BigDecimal("50.00")));
+        when(mockDealRepository.findByDealUniqueId("TFGHU789YH")).thenReturn(optionalDeal);
+
+
+        final Deal result = dealServiceImplUnderTest.getDealByDealUniqueId("TFGHU789YH");
+
+        assertThat(result).isEqualTo(expectedResult);
+        verify(mockDealRepository, times(1)).findByDealUniqueId("TFGHU789YH");
+    }
+
+    @Test
+    void testGetDealByDealUniqueId_DealRepositoryReturnsAbsent() {
+
+        when(mockDealRepository.findByDealUniqueId("TFGHU789YH")).thenReturn(Optional.empty());
+
+        assertThrows(FxDealNotFoundException.class, () -> dealServiceImplUnderTest.getDealByDealUniqueId("TFGHU789YH"));
+        verify(mockDealRepository, times(1)).findByDealUniqueId("TFGHU789YH");
+    }
+
+    @Test
+    void testRetrieveFXDeal() {
+
+        final APIResponse<DealDto> expectedResult = new APIResponse<>("FX Deal retrieved successfully", time,
+                new DealDto("TFGHU789YH", "EUR", "USD", new BigDecimal("50.00")));
+
+
+        final Optional<Deal> optionalDeal = Optional.of(
+                new Deal(1L, "TFGHU789YH", Currency.getInstance("EUR"), Currency.getInstance("USD"),
+                        time, new BigDecimal("50.00")));
+        when(mockDealRepository.findByDealUniqueId("TFGHU789YH")).thenReturn(optionalDeal);
+
+        final APIResponse<DealDto> result = dealServiceImplUnderTest.retrieveFXDeal("TFGHU789YH");
+
+        assertThat(result.getMessage()).isEqualTo(expectedResult.getMessage());
+        assertThat(result.getDto()).isEqualTo(expectedResult.getDto());
+        verify(mockDealRepository, times(1)).findByDealUniqueId("TFGHU789YH");
+    }
+
+    @Test
+    void testRetrieveFXDeal_DealRepositoryReturnsAbsent() {
+
+        when(mockDealRepository.findByDealUniqueId("TFGHU789YH")).thenReturn(Optional.empty());
+
+        assertThrows(FxDealNotFoundException.class, () -> dealServiceImplUnderTest.retrieveFXDeal("TFGHU789YH"));
+        verify(mockDealRepository, times(1)).findByDealUniqueId("TFGHU789YH");
     }
 }
